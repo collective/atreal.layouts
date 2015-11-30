d6b27b96a37f8533981d0c516d6f1d5eeb031acb
tiazma <tiazma@users.noreply.github.com>
2015-11-30 18:47:03 +0100

    Update setup.py following README file renaming

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0.0dev'
 setup(name='atreal.layouts',
       version=version,
       description="Various layouts for Plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
